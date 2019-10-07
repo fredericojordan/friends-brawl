c673db9c0df40eb7e0f6ca0ca4ebca73be8ebce0
Frederico Jordan <fredericojordan@gmail.com>
2019-10-07 20:51:42 -0300

    Adding map and player

--- src/index.js
+++ src/index.js
@@ -1,32 +1,94 @@
 import Phaser from "phaser";
-import logoImg from "./assets/logo.png";
+import desertTiles from "./assets/tilemaps/tiles/tmw_desert_spacing.png";
+import arenaMap from "./assets/tilemaps/maps/map.json";
+import dude from "./assets/dude.png";
 
-const config = {
+var config = {
   type: Phaser.AUTO,
-  parent: "phaser-example",
+  parent: "friends-brawl",
   width: 800,
-  height: 600,
+  height: 608,
+  physics: {
+    default: "arcade",
+    arcade: {
+      debug: true,
+    }
+  },
   scene: {
     preload: preload,
-    create: create
+    create: create,
+    update: update
   }
 };
 
-const game = new Phaser.Game(config);
+var rt;
+var layer;
+var player;
+var cursors;
 
-function preload() {
-  this.load.image("logo", logoImg);
+var game = new Phaser.Game(config);
+
+function preload ()
+{
+  this.load.image("tiles", desertTiles);
+  this.load.tilemapTiledJSON("map", arenaMap);
+  this.load.spritesheet("dude", dude, {
+    frameWidth: 32,
+    frameHeight: 48,
+  });
+
 }
 
-function create() {
-  const logo = this.add.image(400, 150, "logo");
+function create ()
+{
+  let map = this.make.tilemap({ key: "map" });
+  var tiles = map.addTilesetImage("Desert", "tiles");
+  layer = map.createDynamicLayer("Ground", tiles, 0, 0).setVisible(false);
+  rt = this.add.renderTexture(0, 0, 800, 800);
 
-  this.tweens.add({
-    targets: logo,
-    y: 450,
-    duration: 2000,
-    ease: "Power2",
-    yoyo: true,
-    loop: -1
+  player = this.physics.add.sprite(100, 450, "dude");
+  player.setCollideWorldBounds(true);
+  this.anims.create({
+    key: "left",
+    frames: this.anims.generateFrameNumbers("dude", { start: 0, end: 3 }),
+    frameRate: 10,
+    repeat: -1,
   });
+  this.anims.create({
+    key: "turn",
+    frames: [{ key: "dude", frame: 4 }],
+    frameRate: 20,
+  });
+  this.anims.create({
+    key: "right",
+    frames: this.anims.generateFrameNumbers("dude", { start: 5, end: 8 }),
+    frameRate: 10,
+    repeat: -1,
+  });
+}
+
+function update() {
+  cursors = this.input.keyboard.createCursorKeys();
+
+  player.setVelocityX(0);
+  player.setVelocityY(0);
+
+  if (cursors.left.isDown) {
+    player.setVelocityX(-160);
+    player.anims.play("left", true);
+  } else if (cursors.right.isDown) {
+    player.setVelocityX(160);
+    player.anims.play("right", true);
+  } else {
+    player.anims.play("turn");
+  }
+
+  if (cursors.up.isDown) {
+    player.setVelocityY(-160);
+  } else if (cursors.down.isDown) {
+    player.setVelocityY(160);
+  }
+
+  rt.clear();
+  rt.draw(layer);
 }
